6e674308f1f361f811da0be081e87fa487bad4db
trieu <longgiang9999.gl@gmail.com>
2023-03-14 10:09:35 +0700

    xong phan them 1 so tinh nang

--- modul_1_case_1_function.js
+++ modul_1_case_1_function.js
@@ -22,40 +22,88 @@ class Item {
 }
 let itemList = []
 let valueQuantity =[]
+let priceValue = []
+function resetInput(){
+    document.getElementById('item').value = ''
+    document.getElementById('quantity').value = ''
+    document.getElementById('price').value = ''
+}
 function add(){
+    
+    
     let itemValue = document.getElementById('item').value;
-    itemList.push(itemValue);
+    
     let quantity = document.getElementById('quantity').value;
-    valueQuantity.push(quantity);
-    display();
+    
+    let price = document.getElementById('price').value;
+    
+    
+    if(itemValue !== '' && quantity !== '' && price !== ''){
+        itemList.push(itemValue);
+        valueQuantity.push(quantity);
+        priceValue.push(price);
+    }else {
+        alert('Chưa nhập thông tin')
+    }
+    
+    
+    display()
+    resetInput()
+}
+function toUpparCase(){
+    let str = document.getElementById('item').value.toLowerCase()
+    document.getElementById('item').value = str.charAt(0).toUpperCase() +str.slice(1)
 }
 function display() {
     let display = `<tr>
   <th>STT</th>
-  <th>Ten</th>
-  <th>So luong</th>
-  <th>Sua</th>
-  <th>Xoa</th>
+  <th>Tên</th>
+  <th>Số lượng</th>
+  <th>Giá</th>
+  <th>Sửa</th> 
+  <th>Xóa</th>
 </tr>`
     for (let i = 0; i < itemList.length; i++) {
         display += `<tr>
   <td>${i + 1}</td>
   <td>${itemList[i]}</td>
   <td>${valueQuantity[i]}</td>
-  <td><input onclick="edit(${i})" type="button" value="Sua"></td>
-  <td><input onclick="deleteItem(${i})" type="button" value="Xoa"></td>
+  <td>${priceValue[i]}</td>
+  <td><input onclick="edit(${i})" type="button" value="Sửa"></td>
+  <td><input onclick="deleteItem(${i})" type="button" value="Xóa"></td>
 </tr>`
     }
     document.getElementById('div').innerHTML = `<table>${display}</table>`;
 }
 function edit(index) {
-    itemList[index] = prompt('sua ten');
-    valueQuantity[index] = prompt('nhap so luong');
+    document.getElementById('item').value = itemList[index]
+    document.getElementById('quantity').value = valueQuantity[index]
+    document.getElementById('price').value = priceValue[index]
+    document.getElementById('index').value = [index]
+    document.getElementById('save').style.display = 'none'
+    document.getElementById('update').style.display = 'inline-block'
+    // itemList[index] = prompt('Sửa tên');
+    // valueQuantity[index] = prompt('Nhập số lượng');
+    // priceValue[index] = prompt('Nhập giá')
     display()
 }
+function updateItem(){
+    let index = document.getElementById('index').value;
+    itemList[index] = document.getElementById('item').value;
+    valueQuantity[index] = document.getElementById('quantity').value
+    
+    priceValue[index] = document.getElementById('price').value
+    
+    document.getElementById('save').style.display = 'inline-block'
+    document.getElementById('update').style.display = 'none'
+    display()
+    resetInput()
+}
+
 function deleteItem(index) {
     itemList.splice(index, 1);
     valueQuantity.splice(index,1);
+    priceValue.splice(index,1)
     display()
 }
 
@@ -70,30 +118,6 @@ function deleteItem(index) {
 
 
 
-
-
-
-// function display(){
-//     let display = `<tr>
-//   <th>stt</th>
-//   <th>item</th>
-//   <th>quantity</th>
-//   <th>edit</th>
-//   <th>delete</th>
-// </tr>
-// `
-//     for (let i = 0; i <display.length ; i++) {
-//         display += `<tr>
-//   <th>stt</th>
-//   <th>item</th>
-//   <th>quantity</th>
-//   <th><input type="button" value="edit"></th>
-//   <th><input type="button" value="delete"></th>
-// </tr>
-// `
-//     }
-//     document.getElementById('table').innerHTML = `<table>${display}</table>`;
-// }
 
 
 
